26076d861eda7347c062bfd23b09245e2e19115d
Shikihime <33674466+Shikihime@users.noreply.github.com>
2017-12-06 15:00:51 +0900

    Revert "170206 verloren"
    This reverts commit 7ac47103f31d116b47d655b9338d4e0abbf0ff0b.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
-		이것은 {ryan} 노답이다_2q321312312321321324y234237462378462378
-=======
 		이것은 {ryan} 노답이다
->>>>>>> parent of 7ac4710... 170206 verloren
       </div>
     );
   }
