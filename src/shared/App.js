9670180dfdbcb08ea9436127a43264cfa464f215
khsin0289 <khsin0289@users.noreply.github.com>
2017-12-07 15:02:29 +0900

    20171207_15:00
    Css 전역으로 사용하기 환경설정

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,17 +3,30 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, About } from 'pages';
 import Menu from 'components/Menu';
 
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+const cx = classNames.bind(styles);
+
+console.log(styles); // 콘솔에 뭐가 프린트되는지 확인해보세요!
+
 class App extends Component {
 	render() {
+		const isBlue = true;
+
 		return (
-			<div>
+			/*<div>
 				<Menu/>
 				<Route exact path="/" component={Home}/>
 				<Switch>
 					<Route path="/about/:name" component={About}/>
 					<Route path="/about" component={About}/>
 				</Switch>
-			</div>
+			</div>*/
+			<div className={cx('box', {
+				blue: isBlue
+			})}>
+			</div>			
+			
 		);	
 	}
 }
